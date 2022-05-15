c463a67bda473c76009ea7126534164229bb6f13
Your Name <you@example.com>
2022-05-16 02:35:47 +0300

    bookings commit

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function run() {
     try {
       await client.connect();
       const serviceCollection = client.db("doctors_portal").collection("services");
+      const bookingCollection = client.db("doctors_portal").collection("bookings");
       
      app.get('/service', async(req, res) => {
         const query = {};
@@ -23,6 +24,53 @@ async function run() {
         const services = await cursor.toArray();
         res.send(services);
      })
+
+
+
+     app.get('/available', async(req, res) =>{
+        const date = req.query.date || 'May 16, 2022';
+
+        const services = await serviceCollection.find().toArray();
+
+        const query = {date: date};
+        const bookings = await bookingCollection.find(query).toArray();
+
+         services.forEach( service => {
+             const serviceBookings = bookings.filter(b => b.treatment === service.name );
+             const booked = serviceBookings.map(s => s.slot);
+             const available = service.slots.filter( s => !booked.includes(s));
+             service.slots = available;
+
+             /* 
+             service.booked = booked; */
+         })
+        res.send(services)
+     })
+
+
+
+     app.get('/booking', async(req, res) =>{
+         const patient = req.query.patient;
+         const query = {patient: patient};
+        const bookings = await bookingCollection.find(query).toArray();
+        res.send(bookings)
+     })
+
+
+
+     app.post('/booking', async(req,res) =>{
+         const booking = req.body;
+         const query = {treatment : booking.treatment, date: booking.date, patient: booking.patient};
+         const exist = await bookingCollection.findOne(query);
+         if (exist){
+             return res.send({successful: false, booking: exist})
+         }
+         const result = await bookingCollection.insertOne(booking);
+         return res.send({
+            success: true, result
+         });
+     })
+
     } finally {
       //await client.close();
     }
