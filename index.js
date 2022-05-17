83a278a9c050128c62e809398f11599642f06341
Your Name <you@example.com>
2022-05-17 14:23:39 +0300

    doctor pos commit

--- index.js
+++ index.js
@@ -37,10 +37,11 @@ async function run() {
     const serviceCollection = client.db("doctors_portal").collection("services");
     const bookingCollection = client.db("doctors_portal").collection("bookings");
     const userCollection = client.db("doctors_portal").collection("users");
+    const doctorCollection = client.db("doctors_portal").collection("doctors");
 
     app.get('/service', async (req, res) => {
       const query = {};
-      const cursor = serviceCollection.find(query);
+      const cursor = serviceCollection.find(query).project({name:1});
       const services = await cursor.toArray();
       res.send(services);
     })
@@ -112,6 +113,14 @@ async function run() {
         success: true, result
       });
 
+    })
+
+
+    app.post('/doctor', async(req, res) =>{
+      const doctor = req.body;
+      const result = await doctorCollection.insertOne(doctor);
+
+      res.send(result);
     })
 
     app.put('/user/:email', async (req, res) => {
